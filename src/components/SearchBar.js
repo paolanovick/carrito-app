e2b7ac6c85c21a13f6616d70920d1f6720f6cb56
Paola Novick <paola.novick@davinci.edu.ar>
2025-09-14 14:57:08 -0300

    index.css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { FaMapMarkerAlt, FaCalendarAlt, FaUsers } from "react-icons/fa";
-import "./SearchBar.css";
+import "./index.css";
 
 const SearchBar = ({ onSearch }) => {
   const [activeTab, setActiveTab] = useState("paquetes");
