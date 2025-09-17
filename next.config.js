c59c58c965a83f99cc62cd8f68eb9714b1151ee2
Paola Novick <paola.novick@davinci.edu.ar>
2025-09-17 19:27:47 -0300

    Agregar configuración Next.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  distDir: "build",
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
 };
 
 module.exports = nextConfig;
